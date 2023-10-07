507431373a0adcc8246d3e0b605c6c659d6ca4ab
Antonio N <antonioenpidev@gmail.com>
2023-10-06 20:05:48 -0500

    Inject GOGO source

--- app/src/main/java/ani/saikou/MainActivity.kt
+++ app/src/main/java/ani/saikou/MainActivity.kt
@@ -13,7 +13,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnticipateInterpolator
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -214,12 +213,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        hello()
-
-    }
-
-    private fun hello(){
-        Toast.makeText(this, "hello :)", Toast.LENGTH_SHORT).show()
     }
 
     //ViewPager
--- app/src/main/java/ani/saikou/parsers/anime/AnimeSources.kt
+++ app/src/main/java/ani/saikou/parsers/anime/AnimeSources.kt
@@ -4,17 +4,22 @@ import ani.saikou.Lazier
 import ani.saikou.lazyList
 import ani.saikou.parsers.BaseParser
 import ani.saikou.parsers.WatchSources
-import ani.saikou.parsers.anime.sources.AnimeDummy
+import ani.saikou.parsers.anime.sources.GOGOAnime
 
 object AnimeSources : WatchSources() {
     override val list: List<Lazier<BaseParser>> = lazyList(
-        "Dummy" to ::AnimeDummy,
+        "GOGO" to ::GOGOAnime,
     )
 }
 
 object HAnimeSources : WatchSources() {
     private val aList: List<Lazier<BaseParser>>  = lazyList(
-        // Here comes hentai anime parsers
+        /*
+        "HentaiMama" to ::HentaiMama,
+        "Haho" to ::Haho,
+        "HentaiStream" to ::HentaiStream,
+        "HentaiFF" to ::HentaiFF,
+        */
     )
 
     override val list = listOf(aList, AnimeSources.list).flatten()
